6e1552711c8059c134339f5c3750be33f4a03a77
Tamzid Israk <tamzidisrak12@gmail.com>
2023-07-11 22:43:11 +0600

    add css and js

--- src/main/java/com/example/springbootsmart_Contact_Manager/controller/UserController.java
+++ src/main/java/com/example/springbootsmart_Contact_Manager/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.springbootsmart_Contact_Manager.modal.User;
 import com.example.springbootsmart_Contact_Manager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,13 +13,19 @@ public class UserController {
 
     @Autowired
     UserService userService;
-    @GetMapping("/test")
-    @ResponseBody
-    public String test(){
-        User user = new User();
-        user.setUserName("israk");
-        user.setEmail("israk@gmail.com");
-        userService.saveUser(user);
-        return "Success to save";
+
+    @GetMapping("/home")
+    public String home(Model model){
+        model.addAttribute("title","Home-Smart Contact Manager");
+        return "home";
     }
+
+    @GetMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title","About");
+        return "about";
+    }
+
+
+
 }
